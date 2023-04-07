3ea019907712246e254c3e6eb7eaeb7038aa9201
Guilherme Cabral <eu@guilhermecabral.net>
2023-04-07 23:45:41 +0100

    fix formatting

--- nvim/lua/custom/plugins/null-ls.lua
+++ nvim/lua/custom/plugins/null-ls.lua
@@ -3,25 +3,12 @@ return {
     "jose-elias-alvarez/null-ls.nvim",
     config = function()
       local null_ls = require("null-ls")
-      -- Formatting and linting
-      -- https://github.com/jose-elias-alvarez/null-ls.nvim
-      local status_ok, null_ls = pcall(require, "null-ls")
-      if not status_ok then
-        return
-      end
-
-      -- Check supported formatters
-      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
       local formatting = null_ls.builtins.formatting
-
-      -- Check supported linters
-      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
       local diagnostics = null_ls.builtins.diagnostics
 
       local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
       null_ls.setup {
-        debug = false,
+        debug = true,
         sources = {
           -- JS
           formatting.prettierd,
@@ -34,5 +21,5 @@ return {
         },
       }
     end
-  } -- configure formatters & linters
+  }
 }
