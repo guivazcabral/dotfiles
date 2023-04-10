c049ca399006643043f2ef4bf42a0dc31aaf99f8
Guilherme Cabral <eu@guilhermecabral.net>
2023-04-10 10:10:05 +0100

    fix prettier

--- nvim/lua/custom/plugins/null-ls.lua
+++ nvim/lua/custom/plugins/null-ls.lua
@@ -5,13 +5,15 @@ return {
       local null_ls = require("null-ls")
       local formatting = null_ls.builtins.formatting
       local diagnostics = null_ls.builtins.diagnostics
+      local command_resolver = require("null-ls.helpers.command_resolver")
 
-      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
       null_ls.setup {
         debug = true,
         sources = {
           -- JS
-          formatting.prettierd,
+          formatting.prettier.with({
+            dynamic_command = command_resolver.from_node_modules(),
+          }),
           -- Fish
           formatting.fish_indent,
           -- Terraform
