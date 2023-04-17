748733476db3139d1bbcdc1db7014bddfaf158d0
Guilherme Cabral <eu@guilhermecabral.net>
2023-04-18 00:03:03 +0100

    silent auto nvm load

--- fish/functions/load_nvm.fish
+++ fish/functions/load_nvm.fish
@@ -1,7 +1,7 @@
 function load_nvm --on-variable="PWD"
     if test -e ".nvmrc"
-        nvm use
+        nvm use >/dev/null 2>&1
     else if test -e "app/.nvmrc"
-        nvm use `cat app/.nvmrc`
+        nvm use `cat app/.nvmrc` >/dev/null 2>&1
     end
 end
