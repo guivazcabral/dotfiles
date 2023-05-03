c783121cef56ae191f13add7fea362af2afa421c
Guilherme Cabral <eu@guilhermecabral.net>
2023-05-03 14:40:10 +0100

    add custom fish function to autoload zellij layout based on project name

--- fish/custom/zellij.fish
+++ fish/custom/zellij.fish
@@ -1,2 +1,13 @@
-abbr zj 'zellij'
-abbr zjl 'zellij list-sessions'
+abbr zjls 'zellij list-sessions'
+
+function zj
+  set -l dir (basename $PWD)
+  set -l layout_filename (string join "" $dir ".kdl")
+  set -l layout_file (string join "/" "/Users/guilherme/dev/dotfiles/zellij/layouts" $layout_filename)
+
+  if test -e $layout_file
+    zellij --layout $layout_file
+  else
+    zellij
+  end
+end
