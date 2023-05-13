07337b1700a73fb1d7e039afb52e8586c376c76d
Guilherme Cabral <eu@guilhermecabral.net>
2023-05-13 11:33:47 +0100

    refactor general plugins to use opts instead of config fn

--- nvim/lua/custom/plugins/_general.lua
+++ nvim/lua/custom/plugins/_general.lua
@@ -4,24 +4,11 @@ return {
   "tpope/vim-abolish",
   "stevearc/dressing.nvim",
   "kdheepak/lazygit.nvim",
-  {
-    "phaazon/hop.nvim",
-    branch = "v2",
-    opts = {}
-  },
-  {
-    "windwp/nvim-ts-autotag",
-    config = function()
-      require('nvim-ts-autotag').setup()
-    end
-  },
-  {
-    "norcalli/nvim-colorizer.lua",
-    config = function()
-      require('colorizer').setup()
-    end
-  },
   "gaelph/logsitter.nvim",
+  { "phaazon/hop.nvim",            branch = "v2", opts = {} },
+  { "windwp/nvim-ts-autotag",      opts = {}, },
+  { "norcalli/nvim-colorizer.lua", opts = {}, },
+  { 'akinsho/toggleterm.nvim',     opts = {} },
   {
     "SmiteshP/nvim-navbuddy",
     dependencies = {
