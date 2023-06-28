0c67cc73fb318c5317e92c7cf3d7b11e062f8c6c
Gui Cabral <eu@guilhermecabral.net>
2023-06-28 08:53:32 +0200

    fix auto format on save

--- nvim/init.lua
+++ nvim/init.lua
@@ -368,6 +368,19 @@ local on_attach = function(client, bufnr)
   end, { desc = 'Format current buffer with LSP' })
 
   nmap('<leader>f', '<CMD>Format<CR>', 'Format current buffer with LSP')
+
+  local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({ async = true })
+      end,
+    })
+  end
 end
 
 -- Create a command to send messages output to a file
