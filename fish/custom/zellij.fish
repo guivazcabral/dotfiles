8513c22c21a7d45cff2687058c8b2e9c7cb40559
Gui Cabral <eu@guilhermecabral.net>
2023-07-04 15:55:42 +0100

    fix loading layouts after move to submodule

--- fish/custom/zellij.fish
+++ fish/custom/zellij.fish
@@ -7,8 +7,7 @@ abbr zjkillall 'zellij kill-all-sessions'
 function zj
   set -l dir (basename $PWD)
   set -l layout_filename (string join "" $dir ".kdl")
-  set -l layout_file (string join "/" "/Users/guilherme/dev/dotfiles/zellij/layouts" $layout_filename)
-
+  set -l layout_file (string join "/" "/Users/guilherme/dev/dotfiles/zellij/layouts/layouts" $layout_filename)
   if test -e $layout_file
     zellij --layout $layout_file
   else
