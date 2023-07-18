549d8e14d8fca68ba5b80805d6e1118cddd45b5e
Gui Cabral <eu@guilhermecabral.net>
2023-07-18 10:10:24 +0100

    add diffview integration

--- nvim/lua/custom/plugins/neogit.lua
+++ nvim/lua/custom/plugins/neogit.lua
@@ -1,10 +1,20 @@
 return {
   "NeogitOrg/neogit",
-  dependencies = "nvim-lua/plenary.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+  },
   config = function()
     local neogit = require("neogit")
 
-    neogit.setup({ use_telescope = true, kind = "auto" })
+    neogit.setup({
+      use_telescope = true,
+      kind = "auto",
+      integrations = {
+        diffview = true,
+      },
+      disable_insert_on_commit = "auto",
+    })
 
     local group = vim.api.nvim_create_augroup("MyCustomNeogitEvents", { clear = true })
     vim.api.nvim_create_autocmd("User", {
