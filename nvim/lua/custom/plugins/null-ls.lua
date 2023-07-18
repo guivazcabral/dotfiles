765e9bbc1521e617f8803726da271b94288ca0e9
Gui Cabral <eu@guilhermecabral.net>
2023-07-18 13:36:24 +0100

    lint null-ls plugin declaration

--- nvim/lua/custom/plugins/null-ls.lua
+++ nvim/lua/custom/plugins/null-ls.lua
@@ -1,28 +1,26 @@
 return {
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    dependencies = { "jose-elias-alvarez/typescript.nvim" },
-    config = function()
-      local null_ls = require("null-ls")
-      local formatting = null_ls.builtins.formatting
-      local diagnostics = null_ls.builtins.diagnostics
-      local code_actions = null_ls.builtins.code_actions
+  "jose-elias-alvarez/null-ls.nvim",
+  dependencies = { "jose-elias-alvarez/typescript.nvim" },
+  config = function()
+    local null_ls = require("null-ls")
+    local formatting = null_ls.builtins.formatting
+    local diagnostics = null_ls.builtins.diagnostics
+    local code_actions = null_ls.builtins.code_actions
 
-      null_ls.setup({
-        sources = {
-          -- JS
-          diagnostics.eslint_d,
-          formatting.eslint_d,
-          formatting.prettierd,
-          -- Fish
-          formatting.fish_indent,
-          -- Lua
-          formatting.stylua,
-          -- Terraform
-          formatting.terraform_fmt,
-          code_actions.gitrebase,
-        },
-      })
-    end,
-  },
+    null_ls.setup({
+      sources = {
+        -- JS
+        diagnostics.eslint_d,
+        formatting.eslint_d,
+        formatting.prettierd,
+        -- Fish
+        formatting.fish_indent,
+        -- Lua
+        formatting.stylua,
+        -- Terraform
+        formatting.terraform_fmt,
+        code_actions.gitrebase,
+      },
+    })
+  end,
 }
