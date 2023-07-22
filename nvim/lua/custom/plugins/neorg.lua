86814947f4a25bdc762c2671c4c98945742d99ac
Gui Cabral <eu@guilhermecabral.net>
2023-07-22 17:48:53 +0100

    add neorg

---
+++ nvim/lua/custom/plugins/neorg.lua
@@ -0,0 +1,27 @@
+return {
+  "nvim-neorg/neorg",
+  build = ":Neorg sync-parsers",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    require("neorg").setup({
+      load = {
+        ["core.defaults"] = {}, -- Loads default behaviour
+        ["core.concealer"] = {}, -- Adds pretty icons to your documents
+        ["core.completion"] = {
+          config = {
+            engine = "nvim-cmp",
+          },
+        }, -- Loads completion module
+        ["core.keybinds"] = {}, -- Module for managing keybindings with Neorg mode support
+        ["core.dirman"] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              work = "~/dev/datacamp/notes",
+              home = "~/notes",
+            },
+          },
+        },
+      },
+    })
+  end,
+}
