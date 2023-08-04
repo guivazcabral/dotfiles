85cd0f4e97033d4b06794fb68195a251c65b0556
Gui Cabral <eu@guilhermecabral.net>
2023-08-04 21:31:29 +0100

    add pomodoro timer

--- nvim/lua/custom/plugins/_common.lua
+++ nvim/lua/custom/plugins/_common.lua
@@ -9,4 +9,22 @@ return {
   { "norcalli/nvim-colorizer.lua", opts = {} },
   { "akinsho/toggleterm.nvim", opts = {} },
   "davidgranstrom/nvim-markdown-preview",
+  {
+    "dbinagi/nomodoro",
+    config = function()
+      require("nomodoro").setup({
+        work_time = 25,
+        break_time = 5,
+        menu_available = true,
+        texts = {
+          on_break_complete = "TIME IS UP!",
+          on_work_complete = "TIME IS UP!",
+          status_icon = "🍅 ",
+          timer_format = "!%0M:%0S", -- To include hours: '!%0H:%0M:%0S'
+        },
+        on_work_complete = function() end,
+        on_break_complete = function() end,
+      })
+    end,
+  },
 }
