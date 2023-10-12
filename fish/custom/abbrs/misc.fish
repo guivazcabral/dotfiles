26ab9f3c39f429b3938874e8672ec7fe3737068f
Gui Cabral <eu@guilhermecabral.net>
2023-10-12 15:35:23 +0100

    add fish abbrs

--- fish/custom/abbrs/misc.fish
+++ fish/custom/abbrs/misc.fish
@@ -4,6 +4,8 @@ abbr -g nv nvim
 abbr -g awswhoami 'aws sts get-caller-identity'
 abbr -g ggp 'git push origin (__git.current_branch) --force-with-lease'
 abbr -g ls 'exa --icons --group-directories-first'
+abbr -g l 'exa -l --icons --group-directories-first --octal-permissions'
 abbr -g la 'exa -la --icons --group-directories-first --octal-permissions'
 abbr -g lt 'exa -la --icons --group-directories-first --octal-permissions --tree --level=2'
 abbr -g y yarn
+abbr -g scripts 'cat package.json | jq -r ".scripts"'
