c306cabbd62c6d89408d83d5edf4e1f7c00980fc
Gui Cabral <eu@guilhermecabral.net>
2023-11-30 22:43:00 +0000

    update wezterm keybindings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,24 +1,43 @@
 local wezterm = require("wezterm")
 local config = {}
 
-config.hide_tab_bar_if_only_one_tab = true
-config.color_scheme = "Catppuccin Mocha"
-config.window_background_opacity = 1
 config.automatically_reload_config = true
+config.color_scheme = "Catppuccin Mocha"
+config.disable_default_key_bindings = true
+config.hide_tab_bar_if_only_one_tab = true
 config.native_macos_fullscreen_mode = true
+config.window_background_opacity = 1
 config.window_decorations = "RESIZE"
+config.font_size = 12.5
 
 wezterm.font("CommitMono", { weight = "Regular", stretch = "Normal", style = "Normal" }) -- /Users/guilherme/Library/Fonts/CommitMono-400-Regular.otf, CoreText
 wezterm.font("CommitMono", { weight = "Regular", stretch = "Normal", style = "Italic" }) -- /Users/guilherme/Library/Fonts/CommitMono-400-Italic.otf, CoreText
 wezterm.font("CommitMono", { weight = "Bold", stretch = "Normal", style = "Normal" }) -- /Users/guilherme/Library/Fonts/CommitMono-700-Regular.otf, CoreText
 wezterm.font("CommitMono", { weight = "Bold", stretch = "Normal", style = "Italic" }) -- /Users/guilherme/Library/Fonts/CommitMono-700-Italic.otf, CoreText
-config.font_size = 12.5
 
-config.default_prog = {
-  "/opt/homebrew/bin/fish",
-  "-l",
-  "-c",
-  "zellij",
+-- config.default_prog = {
+--   "/opt/homebrew/bin/fish",
+--   "-l",
+--   "-c",
+--   "zellij",
+-- }
+
+local act = wezterm.action
+
+config.keys = {
+  {
+    key = "v",
+    mods = "CMD",
+    action = act.PasteFrom("Clipboard"),
+  },
+  {
+    key = "k",
+    mods = "CMD",
+    action = act.Multiple({
+      act.ClearScrollback("ScrollbackAndViewport"),
+      act.SendKey({ key = "L", mods = "CTRL" }),
+    }),
+  },
 }
 
 local mux = wezterm.mux
