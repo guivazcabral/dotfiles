a3b7285685a92662df03f459cc3e07b62d237a10
Gui Cabral <eu@guilhermecabral.net>
2023-12-07 15:55:24 +0000

    remove clashing abbr

--- fish/custom/abbrs/misc.fish
+++ fish/custom/abbrs/misc.fish
@@ -2,7 +2,6 @@ abbr -g lg lazygit
 abbr -g ld lazydocker
 abbr -g nv nvim
 abbr -g awswhoami 'aws sts get-caller-identity'
-abbr -g ggp 'git push origin (__git.current_branch) --force-with-lease'
 abbr -g ls 'exa --icons --group-directories-first'
 abbr -g l 'exa -l --icons --group-directories-first --octal-permissions'
 abbr -g la 'exa -la --icons --group-directories-first --octal-permissions'
