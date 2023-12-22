0be5dddf6db1b07e5b70c15aa1fd8fd9d0ddee7f
Gui Cabral <eu@guilhermecabral.net>
2023-12-22 11:49:53 +0000

    remove unused binding

--- nvim/init.lua
+++ nvim/init.lua
@@ -305,9 +305,6 @@ local on_attach = function(client, bufnr)
   nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
   nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
-  -- See `:help K` for why this keymap
-  nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-
   -- Lesser used LSP functionality
   nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
   nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
