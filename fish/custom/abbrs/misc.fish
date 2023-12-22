7bebe925b4b7baa5d28dea0748c3e8b80a53d525
Gui Cabral <eu@guilhermecabral.net>
2023-12-22 11:49:39 +0000

    replace exa with eza

--- fish/custom/abbrs/misc.fish
+++ fish/custom/abbrs/misc.fish
@@ -2,9 +2,9 @@ abbr -g lg lazygit
 abbr -g ld lazydocker
 abbr -g nv nvim
 abbr -g awswhoami 'aws sts get-caller-identity'
-abbr -g ls 'exa --icons --group-directories-first'
-abbr -g l 'exa -l --icons --group-directories-first --octal-permissions'
-abbr -g la 'exa -la --icons --group-directories-first --octal-permissions'
-abbr -g lt 'exa -la --icons --group-directories-first --octal-permissions --tree --level=2'
+abbr -g ls 'eza --icons --group-directories-first'
+abbr -g l 'eza -l --icons --group-directories-first --octal-permissions'
+abbr -g la 'eza -la --icons --group-directories-first --octal-permissions'
+abbr -g lt 'eza -la --icons --group-directories-first --octal-permissions --tree --level=2'
 abbr -g y yarn
 abbr -g scripts 'cat package.json | jq -r ".scripts"'
