6611841c515b141efb5c69f3e5008334bfa9bf97
Gui Cabral <eu@guilhermecabral.net>
2024-01-19 12:01:19 +0000

    tweak keymap, add copilot chat

--- nvim/lua/custom/plugins/copilot.lua
+++ nvim/lua/custom/plugins/copilot.lua
@@ -1,39 +1,44 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup({
-      panel = {
-        enabled = false,
-      },
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        debounce = 100,
-        keymap = {
-          accept = "<M-CR>",
-          accept_word = false,
-          accept_line = false,
-          next = "<M-Down>",
-          prev = "<M-Up>",
-          dismiss = "<C-c>",
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        panel = {
+          enabled = false,
         },
-      },
-      filetypes = {
-        ["."] = false,
-        norg = false,
-        cvs = false,
-        gitcommit = false,
-        gitrebase = false,
-        help = false,
-        hgcommit = false,
-        markdown = false,
-        svn = false,
-        yaml = false,
-      },
-      copilot_node_command = "/Users/guilherme/Library/Application Support/fnm/node-versions/v18.16.0/installation/bin/node",
-      server_opts_overrides = {},
-    })
-  end,
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          debounce = 100,
+          keymap = {
+            accept = "<M-CR>",
+            accept_word = false,
+            accept_line = false,
+            next = "<M-Down>",
+            prev = "<M-Up>",
+            dismiss = "<C-c>",
+          },
+        },
+        filetypes = {
+          ["."] = false,
+          norg = false,
+          cvs = false,
+          gitcommit = false,
+          gitrebase = false,
+          help = false,
+          hgcommit = false,
+          markdown = false,
+          svn = false,
+          yaml = false,
+        },
+        copilot_node_command = "/Users/guilherme/Library/Application Support/fnm/node-versions/v18.16.0/installation/bin/node",
+        server_opts_overrides = {},
+      })
+    end,
+  },
+  {
+    "gptlang/CopilotChat.nvim",
+  },
 }
