096fe350033878a85c6686784473e240743d165f
Gui Cabral <eu@guilhermecabral.net>
2024-02-07 15:56:14 +0000

    simplify zellij setup, remove autoload custom layouts

--- fish/custom/zellij.fish
+++ fish/custom/zellij.fish
@@ -1,16 +1,5 @@
-abbr zz zellij
+abbr zj zellij
 abbr zzls 'zellij list-sessions'
 abbr zza 'zellij attach'
 abbr zzkill 'zellij kill-session'
 abbr zzkillall 'zellij kill-all-sessions'
-
-function zj
-  set -l dir (basename $PWD)
-  set -l layout_filename (string join "" $dir ".kdl")
-  set -l layout_file (string join "/" "/Users/guilherme/dev/dotfiles/zellij/layouts/layouts" $layout_filename)
-  if test -e $layout_file
-    zellij --layout $layout_file
-  else
-    zellij
-  end
-end
