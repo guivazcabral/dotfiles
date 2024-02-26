e28dbbf523d319c816f5e43ba3bd8df49e907e05
Gui Cabral <eu@guilhermecabral.net>
2024-02-26 15:53:56 +0000

    update copilot chat

--- nvim/lua/custom/plugins/copilot.lua
+++ nvim/lua/custom/plugins/copilot.lua
@@ -39,6 +39,16 @@ return {
     end,
   },
   {
-    "gptlang/CopilotChat.nvim",
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+      show_help = "yes", -- Show help text for CopilotChatInPlace, default: yes
+      debug = false, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
+      disable_extra_info = "no", -- Disable extra information (e.g: system prompt) in the response.
+      language = "English", -- Copilot answer language settings when using default prompts. Default language is English.
+    },
+    build = function()
+      vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
+    end,
+    event = "VeryLazy",
   },
 }
