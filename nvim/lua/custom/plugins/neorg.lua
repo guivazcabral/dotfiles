23e3c79b8fd8aa7cfa9dffe6cf87dc7e1010047d
Gui Cabral <eu@guilhermecabral.net>
2024-03-28 09:45:35 +0000

    neorg now has built in build script

--- nvim/lua/custom/plugins/neorg.lua
+++ nvim/lua/custom/plugins/neorg.lua
@@ -1,6 +1,5 @@
 return {
   "nvim-neorg/neorg",
-  build = ":Neorg sync-parsers",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     -- check if current folder contains "datacamp" or "notes"
