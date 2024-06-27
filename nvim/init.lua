719b24181416a8d70fd37ac9400865ef65a5d50d
Gui Cabral <eu@guilhermecabral.net>
2024-06-27 11:50:23 +0100

    formatting tweaks

--- nvim/init.lua
+++ nvim/init.lua
@@ -361,20 +361,14 @@ local on_attach = function(client, bufnr)
       })
     end
 
-    nmap("<leader>fa", fix_all, "[F]ix all")
-  end
-
-  local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-  if client.supports_method("textDocument/formatting") then
-    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = augroup,
-      buffer = bufnr,
-      callback = function()
-        vim.lsp.buf.format({ async = true })
-      end,
-    })
+    nmap("<leader>f", function()
+      require("conform").format({ async = true })
+      fix_all()
+    end, "[F]ix all")
+  else
+    nmap("<leader>f", function()
+      require("conform").format({ async = true })
+    end, "[F]ix all")
   end
 end
 
